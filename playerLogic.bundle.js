55aa697e578fc51672a0eb61aba4ed310de21a2e
Akmal Izuddin <izuddin0407@gmail.com>
2022-01-27 12:33:21 +0800

    completed rewriting and refactor all modules and test lol

--- playerLogic.bundle.js
+++ playerLogic.bundle.js
@@ -7,26 +7,60 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/playerLogic/playerLogic.js":
 /*!****************************************!*\
   !*** ./src/playerLogic/playerLogic.js ***!
   \****************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://space-battleship/./src/playerLogic/playerLogic.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"playerLogic\": () => (/* binding */ playerLogic)\n/* harmony export */ });\n\nconst playerLogic = (function() {\n    function computerPlay( boardName ) {\n        let location = Math.floor( Math.random() * 49 );\n\n        if(boardName.boardArray[location].isAttacked === 'empty') {\n            boardName.recordAttack( location );\n        } else {\n            computerPlay();\n        }\n    };\n\n    function playGame( enemyBoard, myBoard, location ) {\n        let turn = 'player';\n\n        if( turn === 'player' ) {\n            enemyBoard.recordAttack( location );\n            return turn = 'computer';\n        } else if( turn === 'computer' ) {\n            computerPlay( myBoard );\n            return turn = 'player';\n        }\n    }\n\n    return {\n        computerPlay,\n        playGame,\n    };\n})();\n\n\n\n//# sourceURL=webpack://space-battleship/./src/playerLogic/playerLogic.js?");
 
 /***/ })
 
 /******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/playerLogic/playerLogic.js"]();
+/******/ 	__webpack_modules__["./src/playerLogic/playerLogic.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
