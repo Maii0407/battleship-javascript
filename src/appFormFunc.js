ba814b82c28213c3c4b746e95be76fe2b39340f2
Akmal Izuddin <izuddin0407@gmail.com>
2022-02-05 20:12:01 +0800

    solved the placeHorizontal collision problem of appFormFunc.next is creating the logic for resetting ship placements, starting a new game and also randomly placing ships for the computer board

--- src/appFormFunc.js
+++ src/appFormFunc.js
@@ -2,16 +2,17 @@ import { appComponents } from "./appComponents";
 
 const appFormFunc = (function() {
     function placeHorizontal( ship ) {
+        const horiBreak = [4,5,6,11,12,13,18,19,20,25,26,27,32,33,34,39,40,41,46,47,48];
+
         let val = Number(document.getElementById( `${ship.name}-position` ).value);
         let val2 = Number( val ) + 1;
         let val3 = Number( val2 ) + 1;
         let val4 = Number( val3 ) + 1;
 
-        //this still needs work
-        if( val4 >= 6 || val4 >= 13 || val4 >= 20 || val4 >= 27 || val4 >= 34 || val4 >= 41 || val4 >= 48 ) {
-            return;
-        } else {
+        if( horiBreak.indexOf( val ) === -1 ) {
             ship.setPosition( [val, val2, val3, Number( val4 )] );
+        } else {
+            return;
         };
     };
 
@@ -29,9 +30,6 @@ const appFormFunc = (function() {
     };
 
     function submitShipLocation( ship ) {
-        const verticalCollision = [28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48];
-        const horizontalCollision = [4,5,6,11,12,13,18,19,20,25,26,27,32,33,34,39,40,41,46,47,48];
-
         if( document.getElementById( `${ship.name}-direction` ).value === 'horizontal' ) {
             placeHorizontal( ship );
         } else if( document.getElementById( `${ship.name}-direction` ).value === 'vertical' ) {
